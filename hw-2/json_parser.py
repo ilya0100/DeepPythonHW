6b7a54e5de2bb21415dbf004b8f36ca02f1f2174
Ilya <mr.ilya868@mail.ru>
2022-09-27 12:49:40 +0300

    add test with mock

--- hw-2/json_parser.py
+++ hw-2/json_parser.py
@@ -1,10 +1,12 @@
+import dataclasses
 import json
 
 
-def parse_json(json_str: str, keyword_callback, required_fields=None, keywords=None):
-    if (required_fields == None or keywords == None):
+def parse_json(json_str: str, keyword_callback,
+               required_fields=None, keywords=None):
+    if (required_fields is None or keywords is None):
         return
-    
+
     json_doc = json.loads(json_str)
 
     for key in json_doc:
@@ -14,8 +16,9 @@ def parse_json(json_str: str, keyword_callback, required_fields=None, keywords=N
                     keyword_callback(keyword)
 
 
+@dataclasses.dataclass
 class KeyWordList:
-    
+
     def __init__(self):
         self.list = []
 
--- hw-2/tests.py
+++ hw-2/tests.py
@@ -1,4 +1,5 @@
 import unittest
+import unittest.mock
 import random
 
 from faker import Faker
@@ -6,6 +7,23 @@ from json_parser import parse_json, KeyWordList
 
 
 class TestPraseJSON(unittest.TestCase):
+
+    @unittest.mock.patch("json_parser.KeyWordList")
+    def test_case_mock(self, key_word_list_mock):
+        fake = Faker()
+
+        json = fake.pydict(10, value_types="str")
+        json_str = str(json).replace("'", '"')
+
+        required_fields = random.sample(list(json.keys()), 5)
+
+        keywords = []
+        for key in required_fields:
+            keywords.append(str(json[key]))
+
+        parse_json(json_str, key_word_list_mock, required_fields, keywords)
+
+        self.assertEqual(key_word_list_mock.call_count, 5)
 
     def test_case_1(self):
         json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
@@ -22,7 +40,7 @@ class TestPraseJSON(unittest.TestCase):
 
         json = fake.pydict(10, value_types="str")
         json_str = str(json).replace("'", '"')
-        
+
         required_fields = random.sample(list(json.keys()), 5)
 
         keywords = []
