df811e9ac7dc55df0e9faf0e3c29c2eda48982bf
Ilya <mr.ilya868@mail.ru>
2022-10-06 16:47:20 +0300

    01: mistakes correction

---
+++ 01/tests.py
@@ -0,0 +1,138 @@
+import io
+from multiprocessing import context
+import sys
+import unittest
+import contextlib
+
+from tic_tac import TicTacGame
+
+
+class TestTicTac(unittest.TestCase):
+    def test_init(self):
+        game = TicTacGame()
+
+        for i in range(3):
+            for j in range(3):
+                self.assertEqual(game.board[i][j], ' ')
+
+        self.assertEqual(game.turn, 0)
+
+    def test_get_coordinates_correct(self):
+        game = TicTacGame()
+
+        test_input = io.StringIO("0 0")
+        sys.stdin = test_input
+
+        self.assertEqual(game.get_coordinates(), (0, 0))
+
+    def test_get_coordinates_incorrect(self):
+        with contextlib.redirect_stdout(None):
+            game = TicTacGame()
+
+            test_input = io.StringIO("1 ²")
+            sys.stdin = test_input
+
+            self.failureException(game.get_coordinates())
+
+            test_input = io.StringIO("2;")
+            sys.stdin = test_input
+
+            self.failureException(game.get_coordinates())
+
+            test_input = io.StringIO("asd")
+            sys.stdin = test_input
+
+            self.failureException(game.get_coordinates())
+
+    def test_validate_input_correct(self):
+        game = TicTacGame()
+
+        for i in range(3):
+            for j in range(3):
+                self.assertTrue(game.validate_input((i, j)))
+
+    def test_make_turn(self):
+        game = TicTacGame()
+
+        for i in range(3):
+            for j in range(3):
+                game.make_turn((i, j))
+
+                if game.turn % 2 == 0:
+                    self.assertEqual(game.board[i][j], 'X')
+                else:
+                    self.assertEqual(game.board[i][j], 'O')
+
+                game.turn += 1
+
+    def test_validate_input_incorrect(self):
+        game = TicTacGame()
+
+        with contextlib.redirect_stdout(None):
+            self.assertFalse(game.validate_input((-1, 0)))
+            self.assertFalse(game.validate_input((1, 6)))
+            self.assertFalse(game.validate_input((-1, 6)))
+
+        for i in range(3):
+            for j in range(3):
+                game.make_turn((i, j))
+
+                self.assertFalse(game.validate_input((i, j)))
+
+    def test_check_winner(self):
+        for i in range(3):
+            game = TicTacGame()
+            self.assertFalse(game.check_winner())
+
+            for j in range(3):
+                game.make_turn((i, j))
+
+            self.assertTrue(game.check_winner())
+
+        for i in range(3):
+            game = TicTacGame()
+            self.assertFalse(game.check_winner())
+
+            for j in range(3):
+                game.make_turn((j, i))
+
+            self.assertTrue(game.check_winner())
+
+        game = TicTacGame()
+        for i in range(3):
+            game.make_turn((i, i))
+
+        self.assertTrue(game.check_winner())
+
+        game = TicTacGame()
+        for i in range(3):
+            game.make_turn((i, 2 - i))
+
+        self.assertTrue(game.check_winner())
+
+    def test_start_game(self):
+        game = TicTacGame()
+
+        test_input = io.StringIO("1 1\n2 1\n0 2\n2 1\n2 0\n2 2\n0 0\n1 2\n")
+        sys.stdin = test_input
+
+        test_output = io.StringIO()
+        sys.stdout = test_output
+
+        self.assertEqual(game.start_game(), "X")
+
+        game = TicTacGame()
+
+        test_input = io.StringIO("1 2\n1 1\n1 0\n2 0\n0 2\n2 2\n2 1\n0 0\n")
+        sys.stdin = test_input
+
+        game.start_game()
+        self.assertEqual(game.start_game(), "O")
+
+        game = TicTacGame()
+
+        test_input = io.StringIO("0 0\n0 1\n0 2\n1 0\n1 1\n1 2\n2 1\n2 0\n2 2")
+        sys.stdin = test_input
+
+        game.start_game()
+        self.assertEqual(game.start_game(), "No one")
---
+++ 01/tic_tac.py
@@ -0,0 +1,86 @@
+class TicTacGame:
+
+    def __init__(self):
+        self.board = []
+
+        for _ in range(3):
+            self.board.append([' '] * 3)
+
+        self.turn = 0
+
+    def show_board(self):
+        for i in range(3):
+            print(self.board[i][0], self.board[i][1],
+                  self.board[i][2], sep='|')
+            if i != 2:
+                print('-' * 5)
+
+    def show_whose_turn(self):
+        if self.turn % 2 == 0:
+            print("X turn")
+        else:
+            print("O turn")
+
+    def validate_input(self, coordinates):
+        if 0 <= coordinates[0] <= 2 and 0 <= coordinates[1] <= 2:
+            return self.board[coordinates[0]][coordinates[1]] == ' '
+
+        print("Coordinates must be in the range 0..2")
+        return False
+
+    def make_turn(self, coordinates):
+        if self.turn % 2 == 0:
+            self.board[coordinates[0]][coordinates[1]] = 'X'
+        else:
+            self.board[coordinates[0]][coordinates[1]] = 'O'
+
+    def start_game(self):
+        while (not self.check_winner() and self.turn < 9):
+            self.show_whose_turn()
+
+            coordinates = self.get_coordinates()
+            if coordinates is None:
+                continue
+
+            if self.validate_input(coordinates):
+                self.make_turn(coordinates)
+                self.turn += 1
+
+            self.show_board()
+            print()
+
+        if self.turn == 9:
+            return "No one"
+        if self.turn % 2 == 0:
+            return 'O'
+        return 'X'
+
+    def check_winner(self):
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] != ' ':
+                return True
+
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] != ' ':
+                return True
+
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != ' ':
+            return True
+
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != ' ':
+            return True
+
+        return False
+
+    def get_coordinates(self):
+        try:
+            coordinates = tuple(map(int, input().split()))
+        except:
+            print("Input is incorrect. Use: 'row' 'column'")
+            print("Example: 0 1")
+            return
+        return coordinates
+
+
+if __name__ == "__main__":
+    game = TicTacGame()
+    print(game.start_game(), "wins!")
